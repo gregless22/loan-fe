45e73c3b3195c3418bfaefaa6d3a7197ca4aa2aa
gregless <gregoryconnolly@gmail.com>
2021-01-06 10:46:10 +1100

    hard coded

--- src/api/store.ts
+++ src/api/store.ts
@@ -21,11 +21,13 @@ async function getPayments(url: string, options: any) {
 
 class API {
   // all of the constants required
-  private loansURL = process.env.VUE_APP_LOANS_URL;
-  private upURL = process.env.VUE_APP_UP_URL;
+  private loansURL = 'http://loan-backend/loans/';
+  private upURL =
+    'https://api.up.com.au/api/v1/transactions?filter%5Btag%5D=LoanPayment';
   private upHeaders = {
     headers: new Headers({
-      Authorization: process.env.VUE_APP_UP_AUTH
+      Authorization:
+        'Bearer up:yeah:NhSasHMass5wnO7RnGM7FxM9DOjSpdrQzvnQxDNO5jM7GcTwVq4b7MwLq2Hely3iSGuIKrEFtGPa60v356Z597pGInWGyNrtSquaiUolCxZtng5s6pUO77dRr2qWH6xy'
     })
   };
   public loans: Loan[] = [];
@@ -37,9 +39,7 @@ class API {
     const loans = getLoans(this.loansURL);
     const payments = getPayments(this.upURL, this.upHeaders);
     const values = await Promise.all([payments, loans]);
-    console.log(values);
-    console.log('Env variable', this.upURL, this.upHeaders);
-
+    console.log('loans', loans, 'payments', payments);
     //combine the values
     values[0].forEach((e: Payment) => {
       values[1].forEach((f: Loan) => {
